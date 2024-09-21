92779ad9dcc1c7a8831cff2c401af8ca000502ce
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-09-21 15:52:17 -0300

    updates

--- server/summarize.js
+++ server/summarize.js
@@ -4,18 +4,18 @@ import { summaryExample } from "./utils/summary.js"
 
 export async function summarize(text) {
   try {
-    //return summaryExample
-
     console.log("Realizando o resumo...")
+    console.log("Texto de entrada:", text) // Adicione este log para verificar o texto de entrada
 
     const generator = await pipeline(
       "summarization",
       "Xenova/distilbart-cnn-12-6"
     )
 
-    console.log("Resumo concluído com sucesso!")
-    console.log(output)
     const output = await generator(text)
+    console.log("Output completo:", output) // Adicione este log para verificar a estrutura do objeto `output`
+
+    console.log("Resumo concluído com sucesso!")
     return output[0].summary_text
   } catch (error) {
     console.log("Não foi possível realizar o resumo", error)
