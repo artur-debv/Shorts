1b5c56f7ffdbfe200e4dc50f77f744421d42d4a7
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-09-21 15:02:02 -0300

    refactor: I updated some download properties of the video transcript

--- server/download.js
+++ server/download.js
@@ -26,5 +26,5 @@ export const download = (videoId) =>
         );
         reject(error);
       })
-      .pipe(fs.createWriteStream("./tmp/audio.mp4"));
+      .pipe(fs.createWriteStream("./tmp/"));
   });
